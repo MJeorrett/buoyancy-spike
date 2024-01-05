5ae59dff7deef755cc4a269b7afa53b67bcec718
Matthew Jeorrett <matthew@clearskylogic.com>
2024-01-05 20:42:02 +0000

    add float schedule loader project

---
+++ tools/FloatScheduleLoader/Program.cs
@@ -0,0 +1,6 @@
+﻿const string filePath = "C:\\Users\\Matthew\\Documents\\Siccar\\BulkUploads\\Tendeka\\EmissionsScoping.csv";
+const string apiBaseUri = "https://localhost:7808/api";
+const string floatScheduleEndpoint = apiBaseUri + "/float-schedule";
+const string projectsEndpoint = apiBaseUri + "/projects";
+const string peopleEndpoint = apiBaseUri + "/persons";
+const string nonProjectTimeTypesEndpoint = apiBaseUri + "/nonprojecttimetypes";
